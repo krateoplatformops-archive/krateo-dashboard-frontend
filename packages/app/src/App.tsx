e35b90667b599466d03c4c9a507655cf0f3720ac
mauro sala <mauro.sala@gmail.com>
2021-11-15 12:17:36 +0100

    feat(app): auth providers by env

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -25,11 +25,65 @@ import { entityPage } from './components/catalog/EntityPage';
 import { searchPage } from './components/search/SearchPage';
 import { Root } from './components/Root';
 
-import { AlertDisplay, OAuthRequestDialog } from '@backstage/core-components';
+import {
+  AlertDisplay,
+  OAuthRequestDialog,
+  SignInPage,
+  SignInProviderConfig,
+} from '@backstage/core-components';
+
 import { createApp, FlatRoutes } from '@backstage/core-app-api';
+
+import {
+  githubAuthApiRef,
+  microsoftAuthApiRef,
+  gitlabAuthApiRef,
+} from '@backstage/core-plugin-api';
+
+const githubProvider: SignInProviderConfig = {
+  id: 'github-auth-provider',
+  title: 'GitHub',
+  message: 'Sign in using GitHub',
+  apiRef: githubAuthApiRef,
+};
+
+const gitlabProvider: SignInProviderConfig = {
+  id: 'gitlab-auth-provider',
+  title: 'GitLab',
+  message: 'Sign in using GitLab',
+  apiRef: gitlabAuthApiRef,
+};
+
+const microsoftProvider: SignInProviderConfig = {
+  id: 'microsoft-auth-provider',
+  title: 'Microsoft',
+  message: 'Sign in using Microsoft',
+  apiRef: microsoftAuthApiRef,
+};
+
+/* AUTH */
+const enabledProviders: any = [];
+
+if (process.env.AUTH_GUEST === 'true') {
+  enabledProviders.push('guest');
+}
+if (process.env.AUTH_GITHUB_CLIENT_ID !== '') {
+  enabledProviders.push(githubProvider);
+}
+if (process.env.AUTH_GITLAB_CLIENT_ID !== '') {
+  enabledProviders.push(gitlabProvider);
+}
+if (process.env.AUTH_MICROSOFT_CLIENT_ID !== '') {
+  enabledProviders.push(microsoftProvider);
+}
 
 const app = createApp({
   apis,
+  components: {
+    SignInPage: props => (
+      <SignInPage {...props} auto providers={enabledProviders} />
+    ),
+  },
   bindRoutes({ bind }) {
     bind(catalogPlugin.externalRoutes, {
       createComponent: scaffolderPlugin.routes.root,
