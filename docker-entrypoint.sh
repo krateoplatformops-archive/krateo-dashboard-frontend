bd0b4d1a9b621e0fe7aac99089964c9f368151cc
mauro sala <mauro.sala@gmail.com>
2021-12-01 16:11:56 +0100

    fix(docker-entrypoint): working dir

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-cd /app
+cd /app/packages/app
+CUR_DIR=$(pwd)
 yarn build
 cp -r /app/packages/app/dist/* /usr/share/nginx/html
 envsubst '\$PORT' < /etc/nginx/conf.d/configfile.template > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'
